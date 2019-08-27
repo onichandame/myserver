64d1ba379198efefbb3f8b403cbc2fb529f50bb3
Zhang Xiao <zhangxiao@neuboron.com>
2019-08-27 10:40:31 +0800

    register

--- public/form.auth.js
+++ public/form.auth.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
   $('input[name=username]').change(function(){
     if($('input[name=username]').val().length<7){
       $('#submit').attr('disabled',true)
+      $('#username-warning').html()
+      var popper=new Popper($('input[name=username]'),$('#username-warning'),{placement:right,
+      })
+      $()
     }else{
       $('#submit').removeAttr('disabled')
     }
