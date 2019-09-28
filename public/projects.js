854df22f76581e076150500c8be0a158e74466e1
onichandame <zxinmyth@gmail.com>
2019-09-28 15:52:27 +0800

    done homepage

--- public/projects.js
+++ public/projects.js
@@ -11,19 +11,12 @@ $(document).ready(function(){
 function updatepro(str){
   try{
     var pro=JSON.parse(str)
-    const parent=$('#projects').outerWidth()
-    var current=parent
     pro.forEach((item)=>{
-      if(current>=parent){
-        $('#projects').append('<div class="row"></div>')
-        current=0
-      }
-      $('#projects .row:last-child').append(additem(item))
-      current+=$('#projects .row:last-child .card:last-child').outerWidth()
+      $('.grid').append(additem(item))
     })
   }catch(e){
   }
 }
 function additem(pro){
-  return '<div class="col-xs-12 col-lg-6 justify-content-center"><a class="card" href="'+pro.link+'"><img class="card-img-top" src="'+pro.icon+'"><div class="card-body"><h5 class="card-title">'+pro.title+'</h5><p class="card-title">'+pro.desc+'</p></div></a></div>'
+  return '<li class="col-xs-12 col-lg-6 justify-content-center"><a class="nav-link card mx-auto" href="'+pro.link+'"><img class="card-img-top" src="'+pro.icon+'"><div class="card-body"><h5 class="card-title">'+pro.title+'</h5><p class="card-text">'+pro.desc+'</p></div></a></li>'
 }
