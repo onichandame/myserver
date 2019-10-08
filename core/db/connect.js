ec5e43aeeb7f05d8106a22fe09739e054d799941
Zhang Xiao <zhangxiao@neuboron.com>
2019-10-08 16:00:43 +0800

    fixed db bugs

--- core/db/connect.js
+++ core/db/connect.js
@@ -5,8 +5,10 @@ const config=require(path.resolve(__dirname,'config.js'))
 function connect(){
   return config()
   .then((p)=>{
-    var db=new sqlite3.Database(p.name,sqlite3.OPEN_READWRITE|sqlite3.OPEN_CREATE,(err)=>{
+    var db=new sqlite3.Database(path.resolve(p.path,p.name),sqlite3.OPEN_READWRITE|sqlite3.OPEN_CREATE,(err)=>{
       return db
     })
   })
 }
+
+module.exports=connect
