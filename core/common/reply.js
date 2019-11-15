aed4e1693a8647426dd30cce028b7b6b1e71f927
onichandame <zxinmyth@gmail.com>
2019-11-15 22:52:13 +0800

    group core into new repo

--- core/common/reply.js
+++ core/common/reply.js
@@ -1,3 +1,18 @@
+/* takes res and sends it based on properties:
+ *
+ * status
+ * statusCode: HTTP status. default 500
+ *
+ * header
+ * set before calling this function
+ *
+ * body
+ * res.page: path of the file being rendered
+ * res.body: JSON/string/Number being sent
+ * none of above: send without body
+ *
+ * resolves res on success, log error
+ */
 module.exports=function(res){
   return new Promise((resolve,reject)=>{
     if(!res.statusCode) res.status(500)
