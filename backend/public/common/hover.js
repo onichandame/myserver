ce6466dbb23bd2ee62d7d463d832cf4c34d7cad4
onichandame <zxinmyth@gmail.com>
2020-01-01 21:32:27 +0800

    added central;

--- backend/public/common/hover.js
+++
@@ -1,9 +0,0 @@
-$('.dropdown').hover(function(){
-  $(this).addClass('show')
-  $(this).find('[data-toggle="dropdown"]').attr('aria-expanded',true)
-  $(this).find('.dropdown-menu').addClass('show')
-},function(){
-  $(this).removeClass('show')
-  $(this).find('[data-toggle="dropdown"]').attr('aria-expanded',false)
-  $(this).find('.dropdown-menu').removeClass('show')
-})
